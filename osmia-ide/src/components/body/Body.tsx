8cd758d8780708c2f659e1e24ec9f5f17a20aefa
jkutkut <45950243+Jkutkut@users.noreply.github.com>
2024-03-06 23:08:44 +0100

    language selector

--- osmia-ide/src/components/body/Body.tsx
+++ osmia-ide/src/components/body/Body.tsx
@@ -1,15 +1,53 @@
-import {useContext, useEffect, useState} from "react";
+import {useContext, useEffect, useRef, useState} from "react";
 import FileContext from "../../context/FileContext";
 import FileSelector from "./fileSelector/FileSelector";
 import Editor from '@monaco-editor/react';
 
+
+const files = {
+  'script.js': {
+    selectorName: "JavaScript",
+    name: 'script.js',
+    language: 'javascript',
+    value: "console.log('Hello, world!');",
+  },
+  'ctx.json': {
+    selectorName: "JSON",
+    name: 'ctx.json',
+    language: 'json',
+    value: "{\n\t\"key\": \"value\"\n}"
+  },
+  'style.css': {
+    selectorName: "CSS",
+    name: 'style.css',
+    language: 'css',
+    value: "body { background-color: #333; }",
+  },
+  'index.html': {
+    selectorName: "HTML",
+    name: 'index.html',
+    language: 'html',
+    value: '<h1>Hello, world!</h1>',
+  },
+  'plain.txt': {
+    selectorName: "Plain",
+    name: 'plain.txt',
+    language: 'plaintext',
+    value: "Hello, world!",
+  },
+};
+
+const filesArray = Object.entries(files).map(([_, file]) => file);
 
 const Body = () => {
   const FILE_SELECTOR = -1;
   const { tabIndex } = useContext(FileContext);
   const [ height, setHeight ] = useState(0);
 
-  const [ language, setLanguage ] = useState('javascript');
+  // const editorRef = useRef<any>(null);
+  // const [ language, setLanguage ] = useState("js");
+  const [ fileName, setFileName ] = useState<string>("script.js");
+  const file = files[fileName];
 
   useEffect(() => {
     const updateHeight = () => {
@@ -21,42 +59,37 @@ const Body = () => {
     return () => window.removeEventListener('resize', updateHeight);
   });
 
-  const handleEditorWillMount = (monaco: any) => {
-    console.log("before mount", monaco);
-  };
-
-  const handleEditorDidMount = (editor: any, monaco: any) => {
-    console.log("after mount", editor, monaco);
-  };
-
-  const handleEditorChange = (value: string | undefined, event: any) => {
-    console.log("value changed", value, event);
-  };
-
-  const handleEditorValidation = (markers: any) => {
-    console.log("markers", markers);
-  }
-
   return <>
     {tabIndex === FILE_SELECTOR &&
       <FileSelector /> ||
       <div>
-        <select value={language} onChange={e => {
-          setLanguage(e.target.value);
+        <select value={fileName} onChange={e => {
+          setFileName(e.target.value);
         }}>
-          <option value="javascript">JavaScript</option>
-          <option value="html">HTML</option>
-          <option value="css">CSS</option>
+          {filesArray.map((file, idx) => (
+            <option key={idx} value={file.name}>{file.selectorName}</option>
+          ))}
         </select>
         <Editor
           theme="vs-dark"
           height={height}
-          defaultLanguage="javascript"
-          defaultValue="// some comment"
-          onChange={handleEditorChange}
-          onMount={handleEditorDidMount}
-          beforeMount={handleEditorWillMount}
-          onValidate={handleEditorValidation}
+          // path={file.name}
+          // defaultLanguage={file.language}
+          // defaultValue={file.value}
+          language={file.language}
+          // value={file.value}
+          // path={file.name}
+          loading={
+            <div>
+              Loading...
+            </div>
+          }
+          // options={{}}
+          // overrideServices={{}}
+          // onChange={handleEditorChange}
+          // onMount={handleEditorDidMount}
+          // beforeMount={handleEditorWillMount}
+          // onValidate={handleEditorValidation}
         />
       </div>
     }
