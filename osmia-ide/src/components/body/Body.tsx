245ce483a4345862b97f252dac7b15ac04060d8c
jkutkut <45950243+Jkutkut@users.noreply.github.com>
2024-03-09 01:29:05 +0100

    feat editor: language and file selector

--- osmia-ide/src/components/body/Body.tsx
+++ osmia-ide/src/components/body/Body.tsx
@@ -1,53 +1,78 @@
-import {useContext, useEffect, useRef, useState} from "react";
+import {useContext, useEffect, useState} from "react";
+import {version} from "../../../package.json";
+
 import FileContext from "../../context/FileContext";
 import FileSelector from "./fileSelector/FileSelector";
 import Editor from '@monaco-editor/react';
 
+const languages = [
+  "TypeScript",
+  "JavaScript",
+  "CSS",
+  "LESS",
+  "SCSS",
+  "JSON",
+  "HTML",
+  "XML",
+  "PHP",
+  "C#",
+  "C++",
+  "Razor",
+  "Markdown",
+  "Diff",
+  "Java",
+  "VB",
+  "CoffeeScript",
+  "Handlebars",
+  "Batch",
+  "Pug",
+  "F#",
+  "Lua",
+  "Powershell",
+  "Python",
+  "Ruby",
+  "SASS",
+  "R",
+  "Objective-C"
+].map(l => l.toLowerCase());
+languages.sort((a, b) => a.localeCompare(b));
 
-const files = {
-  'script.js': {
-    selectorName: "JavaScript",
-    name: 'script.js',
-    language: 'javascript',
-    value: "console.log('Hello, world!');",
-  },
-  'ctx.json': {
-    selectorName: "JSON",
-    name: 'ctx.json',
-    language: 'json',
-    value: "{\n\t\"key\": \"value\"\n}"
-  },
-  'style.css': {
-    selectorName: "CSS",
-    name: 'style.css',
-    language: 'css',
-    value: "body { background-color: #333; }",
-  },
-  'index.html': {
-    selectorName: "HTML",
-    name: 'index.html',
-    language: 'html',
-    value: '<h1>Hello, world!</h1>',
-  },
-  'plain.txt': {
-    selectorName: "Plain",
-    name: 'plain.txt',
-    language: 'plaintext',
-    value: "Hello, world!",
-  },
+type EditorFile = {
+  name: string;
+  language: string;
+  value: string;
 };
-
-const filesArray = Object.entries(files).map(([_, file]) => file);
 
 const Body = () => {
   const FILE_SELECTOR = -1;
-  const { tabIndex } = useContext(FileContext);
+  const { tabIndex, openFiles } = useContext(FileContext);
   const [ height, setHeight ] = useState(0);
 
-  // const editorRef = useRef<any>(null);
-  // const [ language, setLanguage ] = useState("js");
-  const [ fileName, setFileName ] = useState<string>("script.js");
-  const file = files[fileName];
+  const [ editorFiles, setEditorFiles ] = useState<EditorFile[]>([
+    {
+      name: 'osmia',
+      language: 'html',
+      value: "<h1>Hello, world!</h1>\n<p>This is the osmia code editor.</p>"
+    },
+    {
+      name: 'ctx',
+      language: 'json',
+      value: `{\n\t\"program\": \"${openFiles[tabIndex]?.name}\",\n\t\"version\": \"${version}\"\n}`
+    },
+    {
+      name: 'result',
+      language: 'javascript',
+      value: "// Result will be here"
+    }
+  ]);
+  const [ activeFile, setActiveFile ] = useState(0);
+
+  useEffect(() => {
+    setEditorFiles(fs => {
+      fs[1].value = `{\n\t\"program\": \"${openFiles[tabIndex]?.name}\",\n\t\"version\": \"${version}\"\n}`;
+      return fs;
+    });
+  }, [tabIndex]);
 
   useEffect(() => {
     const updateHeight = () => {
@@ -63,22 +88,50 @@ const Body = () => {
     {tabIndex === FILE_SELECTOR &&
       <FileSelector /> ||
       <div>
-        <select value={fileName} onChange={e => {
-          setFileName(e.target.value);
-        }}>
-          {filesArray.map((file, idx) => (
-            <option key={idx} value={file.name}>{file.selectorName}</option>
-          ))}
-        </select>
+        <div className="d-flex w-100 p-2">
+          <div className="d-flex flex-row col-8 align-items-center">
+            {editorFiles.map((file, idx) => (
+              <button
+                key={idx}
+                className="btn"
+                disabled={idx === activeFile}
+                onClick={() => setActiveFile(idx)}
+              >
+                {file.name}
+              </button>
+            ))}
+          </div>
+          <div className="col-4">
+            <div className="d-flex flex-row align-items-center">
+              <span className="flex-nowrap w-50">
+                Active language: <i>{editorFiles[activeFile].language}</i>
+              </span>
+              <select
+                className="form-select w-50"
+                value={editorFiles[activeFile].language}
+                onChange={e => {
+                  const language = e.target.value;
+                  setEditorFiles(prev => {
+                    prev[activeFile].language = language;
+                    return [...prev];
+                  });
+                }}
+              >
+                {languages.map((language, idx) => (
+                  <option key={idx} value={language}>{language}</option>
+                ))}
+              </select>
+            </div>
+          </div>
+        </div>
         <Editor
           theme="vs-dark"
           height={height}
-          // path={file.name}
-          // defaultLanguage={file.language}
-          // defaultValue={file.value}
-          language={file.language}
-          // value={file.value}
-          // path={file.name}
+          
+          path={editorFiles[activeFile].name}
+          language={editorFiles[activeFile].language}
+          defaultValue={editorFiles[activeFile].value}
+          
           loading={
             <div>
               Loading...
